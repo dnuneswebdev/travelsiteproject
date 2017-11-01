e16aa6a279a178e50743aa09d541e8d86c899396
Daniel <dnuneswebdev@gmail.com>
2017-11-01 16:47:25 -0200

    Completed desktop and mobile header styles.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,13 +5,14 @@ var cssVars = require("postcss-simple-vars");
 var cssNested = require("postcss-nested");
 var cssImport = require("postcss-import");
 var cssMixins = require("postcss-mixins");
+var cssHexRgba = require("postcss-hexrgba");
 
 
 
 
 gulp.task("styles", function() {
     return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, autoPreFixer]))
+    .pipe(postcss([cssImport, cssMixins, cssVars, cssNested, cssHexRgba, autoPreFixer]))
     .on("error", function(errorInfo) {
         console.log(errorInfo.toString());
 
